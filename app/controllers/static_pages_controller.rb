3ceaa2caf8f09ba7944d3f7915705d49ebd0dd9e
Wan <your.email@example.com>
2017-07-07 13:55:11 +0800

    Set validation parameters for the form before sub to db, flash message not functioning tbs later

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,17 @@ class StaticPagesController < ApplicationController
 	@booking = Booking.new
   end
   def create
-	@booking = Booking.new(booking_params)
+	@booking = Booking.create(booking_params)
 	if @booking.save
 		flash[:success] = "Booking Made"
-		redirect_to 'root_url'
+		redirect_to root_url
 	else
 		flash.now[:danger] = "Unable to Book"
-		redirect_to 'root_url'
+		redirect_to root_url
 	end
   end
+   private
+	def booking_params
+		params.require(:booking).permit(:name, :phone_no, :email, :booking_date, :booking_ses, :serv_loc, :car_make)
+	end
 end
