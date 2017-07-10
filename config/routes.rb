0fb86ae690f8f371d2095626c525a15eba695a8a
Wan <your.email@example.com>
2017-07-10 16:33:19 +0800

    Added admin access and limit access

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'static_pages#home'
   get '/book', to: 'static_pages#booking'
   post '/book', to: 'static_pages#create'
   get '/mng', to: 'static_pages#management'
   get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'	
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'	
   resources :users
   resources :static_pages do
 	member do
